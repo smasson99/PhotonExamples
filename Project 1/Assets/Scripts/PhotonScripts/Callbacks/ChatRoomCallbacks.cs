540158725f7e5c5f4907b5ac036dbfec112410a4
smasson <smasson@cimmi.ca>
2019-06-07 12:11:43 -0400

    [master]Bugfix and refactor of the callback system.

---
+++ Project 1/Assets/Scripts/PhotonScripts/Callbacks/ChatRoomCallbacks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChatRoomCallbacks : MonoBehaviourPunCallbacks
+{
+    private const string DefaultOfflineRoomName = "Offline";
+
+    [SerializeField]
+    private string offlineRoomName = DefaultOfflineRoomName;
+
+    [SerializeField]
+    private OnLeaveRoomButtonClicked onLeaveRoomButtonClicked = null;
+
+    private void Awake()
+    {
+        VerifySerializeFields();
+    }
+
+    private void VerifySerializeFields()
+    {
+        if (onLeaveRoomButtonClicked is null)
+        {
+            throw new NullReferenceException(nameof(onLeaveRoomButtonClicked));
+        }
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+
+        onLeaveRoomButtonClicked.OnPublished += OnLeaveRoomButtonClicked;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+
+        onLeaveRoomButtonClicked.OnPublished -= OnLeaveRoomButtonClicked;
+    }
+
+    private void OnLeaveRoomButtonClicked()
+    {
+        LeaveRoom();
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom()
+    {
+        SceneManager.LoadScene(offlineRoomName);
+    }
+}
--- Project 1/Assets/Scripts/PhotonScripts/Callbacks/MainMenuCallbacks.cs
+++ Project 1/Assets/Scripts/PhotonScripts/Callbacks/MainMenuCallbacks.cs
@@ -4,7 +4,7 @@ using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneManager_PhotonCallBacks : MonoBehaviourPunCallbacks
+public class MainMenuCallbacks : MonoBehaviourPunCallbacks
 {
     private const byte DefaultAmountOfPlayersPeerRoom = 5;
     private const string DefaultOnlineRoomName = "Room";
@@ -14,22 +14,19 @@ public class SceneManager_PhotonCallBacks : MonoBehaviourPunCallbacks
     public const byte DefaultMaxAmountOfPlayers = 20;
 
     [SerializeField]
-    [Range(1, 20)]
-    private byte maxAmountOfPlayersPeerRoom = 5;
+    [Range(DefaultMinAmountOfPlayers, DefaultMaxAmountOfPlayers)]
+    private byte maxAmountOfPlayersPeerRoom = DefaultAmountOfPlayersPeerRoom;
 
     [SerializeField]
     private string onlineRoomName = DefaultOnlineRoomName;
-    
+
     [SerializeField]
     private string offlineRoomName = DefaultOfflineRoomName;
-    
+
     [SerializeField]
     private OnJoinRoomButtonClicked onJoinRoomButtonClicked = null;
-    
-    [SerializeField]
-    private OnLeaveRoomButtonClicked onLeaveRoomButtonClicked = null;
 
-    private bool isConnecting = true;
+    private bool isConnecting;
 
     private void Awake()
     {
@@ -41,43 +38,32 @@ public class SceneManager_PhotonCallBacks : MonoBehaviourPunCallbacks
         if (onJoinRoomButtonClicked is null)
         {
             throw new NullReferenceException(nameof(onJoinRoomButtonClicked));
-        }if (onLeaveRoomButtonClicked is null)
-        {
-            throw new NullReferenceException(nameof(onLeaveRoomButtonClicked));
         }
     }
 
     public override void OnEnable()
     {
         base.OnEnable();
-        
+
         onJoinRoomButtonClicked.OnPublished += OnJoinRoomButtonClicked;
-        onLeaveRoomButtonClicked.OnPublished += OnLeaveRoomButtonClicked;
     }
 
     public override void OnDisable()
     {
         base.OnDisable();
-        
+
         onJoinRoomButtonClicked.OnPublished -= OnJoinRoomButtonClicked;
-        onLeaveRoomButtonClicked.OnPublished -= OnLeaveRoomButtonClicked;
     }
 
     private void OnJoinRoomButtonClicked()
     {
-        Debug.Log("Joins the room");
         Connect();
-    }
-    
-    private void OnLeaveRoomButtonClicked()
-    {
-        Debug.Log("Leaves the Room");
-        LeaveRoom();
     }
 
     public void Connect()
     {
         isConnecting = true;
+        
         PhotonNetwork.ConnectUsingSettings();
     }
 
@@ -91,13 +77,8 @@ public class SceneManager_PhotonCallBacks : MonoBehaviourPunCallbacks
                 MaxPlayers = maxAmountOfPlayersPeerRoom
             };
 
-            PhotonNetwork.JoinOrCreateRoom("Room1", roomOptions, TypedLobby.Default);
+            PhotonNetwork.JoinOrCreateRoom("ChatRoom", roomOptions, TypedLobby.Default);
         }
-    }
-
-    public void LeaveRoom()
-    {
-        PhotonNetwork.LeaveRoom();
     }
 
     public override void OnLeftRoom()
